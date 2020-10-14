cf34019d2f854d22fa1b256ce3e2454f4bc92e3f
João Vasconcelos <jvsn19@gmail.com>
2020-10-14 16:13:15 -0300

    Adding some style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logo from './images/Logo.svg'
+
 function App() {
   return (
-    <h1> Hello World </h1>
+    <div id = 'landing-page'>
+      <div className="content-wrapper">
+        <img src={logo} alt=""/>
+
+        <main>
+          <h1>Leve felicidade para o mundo.</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Fortaleza</strong>
+          <span> Ceará</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
